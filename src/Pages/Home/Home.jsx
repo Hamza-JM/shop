300f6778277edd0f724137ba846487011efb40ed
Hamza Jomaa <hamza.jomaa@supcom.tn>
2023-07-20 21:20:17 +0100

    first commit

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,24 +1,43 @@
 import React from "react";
+import Card from "../../components/Card/Card";
 import bannerImg from "../../assets/icons/Banner.svg";
 import "./Home.css";
+function Products(props) {
+  return (
+    <div>
+      <h1>products</h1>
+      {props.products.map((product) => (
+        <Card key={product.name} value={product} />
+      ))}
+    </div>
+  );
+}
 function Home() {
-  //here you can declare the products array => 7/
+  const products = [
+    {
+      name: "Calabrese Broccoli",
+      description:
+        "Calabrese is an old-fashioned variety of broccoli renowned for its bluish green heads and multiple side shoots",
+      oldPrice: 20,
+      newPrice: 13,
+      rate: 5,
+      category: "vegetable",
+    },
+    {
+      name: "Strawberry",
+      description:
+        "The garden strawberry is a widely grown hybrid species of the genus Fragaria, collectively known as the strawberries",
+      oldPrice: 40,
+      newPrice: 25,
+      rate: 5,
+      category: "fruit",
+    },
+  ];
+
   return (
     <div className="home_page">
       <img className="banner_img" src={bannerImg} alt="" />
-      <div className="content">
-        try here
-        {/*
-       code of cards here
-       1/ create a folder named Card in the component folder
-       2/ create Card.jsx and Card.css in the Card component 
-       3/ export the Card component with "export default Card"
-       4/ import the Card component in Home.jsx 
-       5/ put the Card component in the content 
-       6/ in Home.jsx create an array named products of 5 objects each object has  id :string, image :string, name : string , description : string , newPrice : number , oldPrice : number ,category : string , rate : number
-       7/ in Home.jsx in the jsx part (html) map the array of products and in each iteration call the Card Component and pass the object as a props 
-      */}
-      </div>
+      <div className="content">{Products({ products })}</div>
     </div>
   );
 }
